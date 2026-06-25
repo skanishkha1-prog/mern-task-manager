337575941fd319b33e72509732ffca56038b3446
Kanishkha S <skanishkha1@gmail.com>
2026-06-25 12:28:35 +0530

    Update API URL for production

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-});
+baseURL: "https://mern-task-manager-o725.onrender.com/api",});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");

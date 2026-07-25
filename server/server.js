17ffa256d6e91fd4ffafce4220aeaf426f7aeb28
Kanishkha S <skanishkha1@gmail.com>
2026-07-25 10:02:07 +0530

    fix cookie parser initialization

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
-const cookieParser = require("cookie-parser");
 
-app.use(cookieParser());
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
 
 const app = express();
 
